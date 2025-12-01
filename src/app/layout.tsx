086148bb0f71f4e674339068b3f7daf9f10cd0ee
Bedmor <besim.ozdemir@ogr.sakarya.edu.tr>
2025-12-01 23:26:30 +0300

    spelling error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1503967484019606"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <meta
           name="google-adsense-account"
